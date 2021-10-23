8df01716305acda46055725ff17607eeebff5114
De Chirico Francesco <22662277+fraDeckiGH@users.noreply.github.com>
2021-10-23 21:32:16 +0200

    main: before adding auth

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -74,10 +74,32 @@
     
   ]
   
+  let localChangesQueue: any[] = []
+  
+  $: {
+    console.log(`react`, localChangesQueue);
+    (async () => {
+      for await (const el of localChangesQueue) {
+        localChangesQueue.shift()
+        // localChangesQueue = localChangesQueue
+        console.log(`for await`, JSON.parse(JSON.stringify(localChangesQueue)))
+      }
+    })
+  }
+  
+  
+  // ----------------------------
+  // execution
+  
+  initApp()
   
   // createNodes()
-  createPaginationChains()
+  // createPaginationChains()
   // interrogateGun("date")
+  
+  
+  // ----------------------------
+  // functions
   
   function createNodes() {
     // console.log('createNodes()')
@@ -138,133 +160,6 @@
     })
   }
   
-  async function createPaginationChains() {
-    files.forEach(async (el, i) => {
-      for (const key in el) {
-        // TODO cases to evaluate:
-        // el[key] may be nullish
-        // make distinction based on type: isoString, number, bool, string
-        
-        if (key !== "date") continue
-        
-        // if (i > 0) break
-        // if (i === 0) continue
-        if (i > 1) break
-        
-        
-        const chainHeadRef = gun
-          .get("File")
-          .get(key)
-        
-        const chainHead = await chainHeadRef
-          // @ts-ignore
-          .count("count", 1) // move somewhere below?
-          .valueCopy_()
-          // .put(elDbRef)
-        // console.log(`chainHead`, JSON.parse(JSON.stringify(chainHead)))
-        console.log(`chainHead`, chainHead)
-        
-        
-        const chainValueRef = gun
-          .get("File")
-          .get(key)
-          .get("values")
-          .get(el[key])
-          // @ts-ignore
-          .count("count", 1)
-        
-        
-        // assumption: both first & last are populated or none of them is
-        if (!chainHead?.first) {
-          // being here means this whole chain is being created right now
-          chainHeadRef.put({
-            first: el[key],
-            last: el[key],
-          })
-          chainValueRef.put({
-            next: el[key],
-            prev: el[key],
-          })
-        } else {
-          // rank first, last & el[key]
-          const values = [
-            chainHead.first,
-            chainHead.last,
-            el[key],
-          ].sort(
-            // comparing numbers?
-            typeof el[key] === "number" ?
-            ((a: number, b: number) => a - b) : undefined
-          )
-          
-          const indexStart = values.indexOf(el[key])
-          const indexEnd = values.lastIndexOf(el[key])
-          
-          if (indexStart === indexEnd) {
-            switch (indexStart) {
-              case 0:
-                chainHeadRef.put({
-                  first: el[key],
-                })
-                // setTimeout(() => {
-                //   console.log(`case0 chainHead`, chainHead)
-                // }, 1000);
-                
-                chainValueRef.put({
-                  next: chainHead.first,
-                  // prev: null, // don't think it's needed
-                })
-                
-                // update the content of the - now former - chain edge
-                gun
-                  .get("File")
-                  .get(key)
-                  .get("values")
-                  .get(chainHead.first)
-                  .put({
-                    prev: el[key],
-                  })
-                
-                break
-              case 1:
-                const chainValue = await chainValueRef
-                  .value_()
-                
-                if (!chainValue.next) {
-                  chainValueRef.put({
-                    next: chainHead.last,
-                  })
-                }
-                if (!chainValue.prev) {
-                  chainValueRef.put({
-                    prev: chainHead.first,
-                  })
-                }
-                break
-              case 2:
-                
-                break
-              default:
-                
-            }
-          } else {
-            if (indexStart === 0 && indexEnd === 1) {
-              
-            } else if (indexStart === 1 && indexEnd === 2) {
-              
-            }
-          }
-          
-          
-        }
-        
-        
-        
-        
-      }
-    })
-  }
-  
   /* function createNodes() {
     // console.log('createNodes()')
     
@@ -295,6 +190,33 @@
       
     })
   } */
+  
+  function enrichEl(el: any, i: number) {
+    // adding fields which allow for proper storage
+    
+    let elToAdd = {
+      _id: i,
+      // _id: nanoid(),
+      
+      // FIXME trading(losing) nanosecond precision for millisecond
+      // TODO change Windows hour 
+      //   testare che Gun.state() rimanga giusto
+      _dateCreated: new Date(gun_state()).toISOString(),
+      _dateUpdated: new Date(gun_state()).toISOString(),
+      
+      // _entityName: "File", // useful?
+    }
+    // elToAdd["_id"] = i
+    // elToAdd["_id"] = nanoid()
+    
+    // key sort needed?
+    elToAdd = {
+      ...elToAdd,
+      ...el,
+    }
+    
+    return elToAdd
+  }
   
   function interrogateGun(path: string) {
     console.log('interrogateGun()')
@@ -328,29 +250,289 @@
     // console.log(`ref`, ref)
   }
   
+  // * changes queue
   
-  // quick tests
+  function queueElem(index: number) {
+    gun
+      .get("changesQueue")
+      .get(`index${index}`)
+      .put(enrichEl(files[index], index))
+  }
   
-  // quickTest()
+  function queueElems() {
+    queueElem(0)
+    queueElem(1)
+  }
+  
+  // * pagination chain
+  
+  /* function createPaginationChains() {
+    files.forEach((el, i) => {
+      paginationChain_elemAdd(el, i)
+    })
+  } */
+  
+  async function paginationChain_elemAdd(el: any, i?: number) {
+    return new Promise(async (resolve) => {
+      for (const key in el) {
+        // TODO cases to evaluate:
+        // el[key] may be nullish
+        // make distinction based on type: isoString, number, bool, string
+        
+        if (key !== "date") continue
+        console.log(`started adding el`, el._id)
+        
+        // if (i > 0) break
+        // if (i === 0) continue
+        // if (i > 1) break
+        
+        
+        const chainHeadRef = gun
+          .get("File")
+          .get(key)
+        const chainHead = await chainHeadRef
+          // @ts-ignore
+          .count("count", 1) // move somewhere below?
+          .valueCopy_()
+          // .put(elDbRef)
+        
+        // might need to place this within a promise
+        const chainValueRef = gun
+          .get("File")
+          .get(key)
+          .get("values")
+          .get(el[key])
+          // @ts-ignore
+          .count("count", 1)
+        
+        
+        // assumption: both first & last are populated or none of them is
+        if (!chainHead?.first) {
+          // being here means this whole chain is being created right now
+          // chainHeadRef.put({
+          //   first: el[key],
+          //   last: el[key],
+          // })
+          await new Promise((resolve) => {
+            chainHeadRef.put({
+              first: el[key],
+              last: el[key],
+            }, () => {
+              resolve(null)
+            })
+          })
+          
+        } else {
+          // rank first, last & el[key]
+          const values = [
+            chainHead.first,
+            chainHead.last,
+            el[key],
+          ].sort(
+            // comparing numbers?
+            typeof el[key] === "number" ?
+            ((a: number, b: number) => a - b) : undefined
+          )
+          
+          const indexStart = values.indexOf(el[key])
+          const indexEnd = values.lastIndexOf(el[key])
+          
+          if (indexStart === indexEnd) {
+            switch (indexStart) {
+              case 0:
+                console.log(`case 0`, )
+                
+                // chainHeadRef.put({
+                //   first: el[key],
+                // })
+                await new Promise((resolve) => {
+                  chainHeadRef.put({
+                    first: el[key],
+                  }, () => {
+                    resolve(null)
+                  })
+                })
+                // setTimeout(() => {
+                //   console.log(`case0 chainHead`, chainHead)
+                // }, 1000);
+                
+                // chainValueRef.put({
+                //   next: chainHead.first,
+                //   // prev: null, // don't think it's needed
+                // })
+                await new Promise((resolve) => {
+                  chainValueRef.put({
+                    next: chainHead.first,
+                    // prev: null, // don't think it's needed
+                  }, () => {
+                    resolve(null)
+                  })
+                })
+                
+                // update the content of the - now former - chain edge
+                // gun
+                //   .get("File")
+                //   .get(key)
+                //   .get("values")
+                //   .get(chainHead.first)
+                //   .put({
+                //     prev: el[key],
+                //   })
+                await new Promise((resolve) => {
+                  gun
+                    .get("File")
+                    .get(key)
+                    .get("values")
+                    .get(chainHead.first)
+                    .put({
+                      prev: el[key],
+                    }, async () => {
+                      resolve(null)
+                    })
+                })
+                break
+              case 1:
+                console.log(`case 1`, )
+                
+                const chainValue = await chainValueRef
+                  .value_()
+                
+                if (!chainValue.next) {
+                  // chainValueRef.put({
+                  //   next: chainHead.last,
+                  // })
+                  await new Promise((resolve) => {
+                    chainValueRef.put({
+                      next: chainHead.last,
+                    }, () => {
+                      resolve(null)
+                    })
+                  })
+                }
+                if (!chainValue.prev) {
+                  // chainValueRef.put({
+                  //   prev: chainHead.first,
+                  // })
+                  await new Promise((resolve) => {
+                    chainValueRef.put({
+                      prev: chainHead.first,
+                    }, () => {
+                      resolve(null)
+                    })
+                  })
+                }
+                break
+              case 2:
+                
+                break
+              default:
+                
+            }
+          } else {
+            if (indexStart === 0 && indexEnd === 1) {
+              
+            } else if (indexStart === 1 && indexEnd === 2) {
+              
+            }
+          }
+          
+          
+        }
+        
+        
+        
+        
+      }
+      
+      console.log(`resolving el._id`, el._id)
+      resolve(null)
+    })
+  }
+  
+  // * start-up
+  
+  // TODO move up
+  
+  
+  function initApp() {
+    gun
+      .get("changesQueue")
+      .map()
+      .once(async (data, key) => {
+        console.log(key, data)
+        
+        // offline mode
+        // #region
+        /* 
+        if a peer goes offline we don't want to do any operations
+        to the pagination chain(s)
+        
+        I'm willing to bet, the way Gun solves conflicts may make 
+        this "changes-queue system" pointless and break the said 
+        pag. chain(s) anyway.
+        
+        Instead find a way to resume pag. chain ops when the user 
+        comes back online
+        */
+        // #endregion 
+        
+        // I might need to shallow copy 'data'
+        // paginationChain_elemAdd(data)
+        localChangesQueue.push(paginationChain_elemAdd(data))
+        localChangesQueue = localChangesQueue
+        
+        // FIXME do this when pagination chain operations are finished
+        // gun
+        //   .get("changesQueue")
+        //   .get(key)
+        //   .put(<any>null)
+      })
+  }
+  
+  // * tests
+  
   async function quickTest() {
     
-    const log = gun
-      .get("changesQueue")
-      .put({
-        
-      })
+    // const log = gun
+    //   .get("changesQueue")
+    //   .get(`index0`)
+    //   .put(files[0])
     
-    console.log(`log`, log)
+    // console.log(`log`, log)
+    
+    await paginationChain_elemAdd(enrichEl(files[0], 1))
+    await paginationChain_elemAdd(enrichEl(files[1], 1))
   }
   
   
   
 </script>
 
+
 <main class="content-container">
-  index
+  <h4>
+    index
+  </h4>
+  
+  <div class="actions-panel">
+    
+    <button on:click="{queueElems}">
+      queue elems
+    </button>
+    <button on:click="{quickTest}">
+      quick test
+    </button>
+    
+  </div>
+  
 </main>
 
+
 <style lang=scss>
+  
+  .actions-panel {
+    display: flex;
+    gap: 1em;
+  }
   
 </style>
