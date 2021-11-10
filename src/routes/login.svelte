c417396b3302a649bde0e67790b5e6765806baa8
De Chirico Francesco <22662277+fraDeckiGH@users.noreply.github.com>
2021-11-10 21:31:53 +0100

    main: about to code w/ firebase (I think)

---
+++ src/routes/login.svelte
@@ -0,0 +1,25 @@
+
+<script lang=ts>
+  
+  
+  
+</script>
+
+
+<main class="content-container">
+  
+  <h4>
+    login
+  </h4>
+  
+  
+  
+  
+</main>
+
+
+<style lang=scss>
+  
+  
+  
+</style>
